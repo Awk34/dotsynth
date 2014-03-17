a2a17ccfc2ff2c451a169959b3b5552aee2500a7
Scott Little <uber5001@gmail.com>
2014-03-17 01:50:46 -0500

    move colors to style.js generated styles

--- dot.js
+++ dot.js
@@ -51,8 +51,8 @@ function Dot(definition, x, y) {
 	this.centerElement.setAttributeNS(null, 'cx', SVG_SIZE/2);
 	this.centerElement.setAttributeNS(null, 'cy', SVG_SIZE/2);
 	this.centerElement.setAttributeNS(null, 'r', DOT_RADIUS);
-	this.centerElement.setAttributeNS(null, 'fill', 'hsla( ' + definition.hue + ', 100%, ' + DOT_LIGHTNESS + ', 1)');
 	this.centerElement.classList.add('dotcenter');
+	this.centerElement.classList.add(definition.className);
 	this.svgElement.classList.add('dotsvg');
 	this.centerElement.object = this;
 	//end Center Dot
@@ -160,9 +160,7 @@ function Dot(definition, x, y) {
 		this.gElement.classList.add('arcWrapper');
 		this.pathElement = document.createElementNS(NS, 'path');
 		this.pathElement.classList.add('arc');
-		this.pathElement.setAttributeNS(null, 'stroke', 'hsla( 0, 0%, ' + EMPTY_ARC_LIGHTNESS + ', ' + EMPTY_ARC_ALPHA + ')');
-		this.pathElement.setAttributeNS(null, 'fill', "none");
-		this.pathElement.setAttributeNS(null, 'stroke-width', ARC_WIDTH);
+		this.pathElement.classList.add('empty');
 		this.pathElement.setAttributeNS(null, 'clip-path', "url(#clip" + clipPathId + ")");
 		
 		//Making the path's 'd'
@@ -210,8 +208,7 @@ function Dot(definition, x, y) {
 		
 		this.indicatorElement = document.createElementNS(NS, 'path');
 		this.indicatorElement.classList.add('arc');
-		this.indicatorElement.setAttributeNS(null, 'fill', 'hsla(' + selfArc.definition.hue + ', 100%, ' + FILL_ARC_LIGHTNESS + ', 1)');
-		this.indicatorElement.setAttributeNS(null, 'stroke', 'hsla(' + selfArc.definition.hue + ', 100%, ' + STROKE_ARC_LIGHTNESS + ', 1)');
+		this.indicatorElement.classList.add(definition.className);
 		this.indicatorElement.style['pointer-events'] = 'none';
 		function drawIndicator(percent) {
 			var str = "M " + selfArc.clipOrigin.x + " " + selfArc.clipOrigin.y;
--- nodes.js
+++ nodes.js
@@ -2,6 +2,7 @@ var DOT_LIST = [
 	{
 		name: "Output",
 		shortName: "Out",
+		className: "output",
 		canTakeInput: true,
 		create: function() {
 			var tmp = context.destination;
@@ -15,6 +16,7 @@ var DOT_LIST = [
 	{
 		name: "Oscillator",
 		shortName: "Osc",
+		className: "oscillator",
 		canTakeInput: false,
 		create: function() {
 			var tmp = context.createOscillator();
@@ -28,6 +30,7 @@ var DOT_LIST = [
 		parameters: [
 			{
 				name: "detune",
+				className: "detune",
 				scale: function(percent) {
 					return (percent-.5)*9600;
 				},
@@ -44,6 +47,7 @@ var DOT_LIST = [
 			},
 			{
 				name: "frequency",
+				className: "frequency",
 				scale: function(percent) {
 					/* Warning: floor function was removed from here. I didn't hear an audible difference.
 					 * I'm not sure why it was here anyway
@@ -69,6 +73,7 @@ var DOT_LIST = [
 	},
 	{
 		name: "Low Frequency Oscillator",
+		className: "lfo",
 		shortName: "LFO",
 		canTakeInput: false,
 		create: function() {
@@ -80,6 +85,7 @@ var DOT_LIST = [
 		parameters: [
 			{
 				name: "frequency",
+				className: "frequency",
 				scale: function(percent) {
 					var d = 128*percent;
 					var f = Math.pow(2,(d-69)/12)*60;
@@ -111,6 +117,7 @@ var DOT_LIST = [
 		parameters: [
 			{
 				name: "gain",
+				className: "gain",
 				scale: function(percent) {
 					return percent;
 				},
@@ -130,6 +137,7 @@ var DOT_LIST = [
 	{
 		name: "Delay",
 		shortName: "Delay",
+		className: "delay",
 		canTakeInput: true,
 		create: function() {
 			return context.createDelay();
@@ -138,6 +146,7 @@ var DOT_LIST = [
 		parameters: [
 			{
 				name: "delayTime",
+				className: "delay",
 				scale: function(percent) {
 					//min: 0.0, max: 1.0
 					return percent;
@@ -175,6 +184,7 @@ var DOT_LIST = [
 	{
 		name: "Compressor",
 		shortName: "Comp",
+		className: "compressor",
 		canTakeInput: true,
 		create: function() {
 			return context.createDynamicsCompressor();
@@ -185,6 +195,7 @@ var DOT_LIST = [
 				// threshold
 				// The decibel value above which the compression will start taking effect. Its default value is -24, with a nominal range of -100 to 0.
 				name: "threshold",
+				className: "threshold",
 				scale: function(percent) {
 					return (percent-1)*100;
 				},
@@ -203,6 +214,7 @@ var DOT_LIST = [
 				// knee
 				// A decibel value representing the range above the threshold where the curve smoothly transitions to the "ratio" portion. Its default value is 30, with a nominal range of 0 to 40.
 				name: "knee",
+				className: "knee",
 				scale: function(percent) {
 					return percent*40;
 				},
@@ -221,6 +233,7 @@ var DOT_LIST = [
 				// ratio
 				// The amount of dB change in input for a 1 dB change in output. Its default value is 12, with a nominal range of 1 to 20.
 				name: "ratio",
+				className: "ratio",
 				scale: function(percent) {
 					return percent*19 + 1;
 				},
@@ -251,6 +264,7 @@ var DOT_LIST = [
 				// attack
 				// The amount of time (in seconds) to reduce the gain by 10dB. Its default value is 0.003, with a nominal range of 0 to 1.
 				name: "attack",
+				className: "attack",
 				scale: function(percent) {
 					return percent;
 				},
@@ -269,6 +283,7 @@ var DOT_LIST = [
 				// release
 				// The amount of time (in seconds) to increase the gain by 10dB. Its default value is 0.250, with a nominal range of 0 to 1.
 				name: "release",
+				className: "release",
 				scale: function(percent) {
 					return percent;
 				},
